4eb27e6a9fcc899b9d2159847373722d535b04e3
Marcelo Becerra <marcelo.becerra@ug.uchile.cl>
2019-12-11 23:56:29 -0300

    Ejemplos del uso de macros

---
+++ tests.rkt
@@ -0,0 +1,57 @@
+#lang play
+
+(require "baseC2.rkt")
+(require "interp_recursive.rkt")
+
+(defmac (foo decision (transitions :
+                                     [state :
+                                            (action → target ...)
+                                            ...]
+                                     ...))
+    #:keywords transitions : →
+      (letrec ([state decision]...) (print state)...))
+
+(defmac (foo2 decision (transitions :
+                                     [state :
+                                            (action → target ...)
+                                            ...]
+                                     ...))
+    #:keywords transitions : →
+      (letrec ([state decision]...) (or state ...)))
+
+(defmac (foo3 decision (transitions :
+                                     [state :
+                                            (action → target ...)
+                                            ...]
+                                     ...))
+    #:keywords transitions : →
+      (letrec ([state (letrec ([action (+ target ...)]...) (max action ...))]
+               ...)
+        (or state ...)) 
+  )
+
+(foo #t (transitions :
+                       [s1 : (1 → 2 3 4)]
+                       [s2 : (1 → 2)
+                           (3 → 2)]
+                       ))
+      
+[test (foo2 #f (transitions :
+                       [s1 : (1 → 2 3 4)]
+                       [s2 : (1 → 2)
+                           (3 → 2)]
+                       ))
+      #f]
+
+[test (foo2 #t (transitions :
+                       [s1 : (1 → 2 3 4)]
+                       [s2 : (1 → 2)
+                           (3 → 2)]
+                       ))
+      #t]
+
+(foo3 #t (transitions :
+                       [s1 : (a1 → 2 3 4)]
+                       [s2 : (a2 → 2)
+                           (a3 → 2 5)]
+                       ))
