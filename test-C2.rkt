01ba2484e4c5ad280f0121a5dd00cb47c73759a3
ValeAAP <43286136+ValeAAP@users.noreply.github.com>
2019-12-12 17:07:09 -0300

    Más automátas

--- test-C2.rkt
+++ test-C2.rkt
@@ -35,7 +35,38 @@
 [test (m'(a b a b)) '(q0 q1 q1 q1 acc1)]
 [test (m'(a b a b a)) #f]
 [test (m'(b a b))'(q0 acc2 acc2 acc2)]
+ ;TEST AUTOMATA DE UN ESTADO sin transiciones
+(define m1 (ND-FSA
+            (init : q0)
+            (final : q0)
+            (transitions : [q0 : ])))
+[test (m1 '()) '(q0)]
+[test (m1 '(a)) #f]
 
+;TEST AUTOMATA DE UN ESTADO con transiciones
+(define m2 (ND-FSA
+            (init : q0)
+            (final : q0)
+            (transitions : [q0 : (a → q0)])))
+[test (m2 '()) '(q0)]
+[test (m2 '(a a a)) '(q0 q0 q0 q0)]
+[test (m2 '(a a b)) #f]
+
+;TEST AUTOMATA CON CANTIDAD PAR DE 0's y 1's
+(define m3 (ND-FSA
+            (init : q0)
+            (final : q0)
+            (transitions : [q0 : (1 → q1)
+                               (0 → q3)]
+                         [q1 : (1 → q0)
+                             (0 → q2)]
+                         [q2 : (1 → q3)
+                             (0 → q1)]
+                         [q3 : (1 → q2)
+                             (0 → q0)])))
+[test (m3 '(1 1 0 0)) '(q0 q1 q0 q3 q0)]
+[test (m3 '(1 0 1 1 0 0 1 0)) '(q0 q1 q2 q3 q2 q1 q2 q3 q0)]
+[test (m3 '(1 1 0 1 0 1 0 0 1)) #f]
 
 #|----------- TEST P2 -----------|#
 ;TEST P2 even
